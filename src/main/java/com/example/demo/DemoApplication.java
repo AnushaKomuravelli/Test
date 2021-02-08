77427b0aa40a3c9621f50f3fdb4ce0c2a3818295
Anusha Komuravelli <anushakomuravelly@gmail.com>
2021-02-08 11:01:58 +0530

    added hello wprld

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		char[] sysout = null;
+		System.out.println(sysout);
 	}
 
 }
